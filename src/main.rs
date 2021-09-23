8f3dc4b414a7e264f5248d5d09b8d940e748f4ca
Alex Black <wa7@sa34.cx>
2021-09-23 15:56:20 +0300

    Add Diesel

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
-fn main() {
-    println!("Hello, world!");
+use dotenv::dotenv;
+use teloxide::{prelude::*, utils::command::BotCommand};
+use std::error::Error;
+
+#[derive(BotCommand)]
+#[command(rename = "lowercase", description = "These commands are supported:")]
+enum Command {
+    #[command(description = "display this text.")]
+    Help,
+    #[command(description = "handle a username.")]
+    Username(String),
+    #[command(description = "handle a username and an age.", parse_with = "split")]
+    UsernameAndAge { username: String, age: u8 },
+}
+
+async fn answer(
+    cx: UpdateWithCx<AutoSend<Bot>, Message>,
+    command: Command,
+) -> Result<(), Box<dyn Error + Send + Sync>> {
+    match command {
+        Command::Help => cx.answer(Command::descriptions()).await?,
+        Command::Username(username) => {
+            cx.answer(format!("Your username is @{}.", username)).await?
+        }
+        Command::UsernameAndAge { username, age } => {
+            cx.answer(format!("Your username is @{} and age is {}.", username, age)).await?
+        }
+    };
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() {
+    teloxide::enable_logging!();
+    log::info!("Starting bot...");
+
+    dotenv().ok();
+    let bot = Bot::from_env().auto_send();
+    let bot_name = "HoldMyBeerBot";
+
+    teloxide::commands_repl(bot, bot_name, answer).await;
 }
