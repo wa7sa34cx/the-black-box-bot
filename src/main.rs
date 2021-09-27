9a629ac8d82d893bc53f826fe70d2e97529aff6b
Alex Black <wa7@sa34.cx>
2021-09-27 16:31:27 +0300

    Just start

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,25 @@
 // See example
 // https://github.com/teloxide/teloxide/tree/dev/examples/sqlite_remember_bot
 
+use dotenv::dotenv;
+use teloxide::{prelude::*, utils::command::BotCommand};
+
 #[tokio::main]
 async fn main() {
     run().await;
 }
 
 async fn run() {
-    // let bar = db::get_bar();
-    // dbg!(bar);
-    println!("hello");
+    // enable .env
+    dotenv().ok();
+    // enable logging
+    teloxide::enable_logging!();
+
+    let bot = Bot::from_env().auto_send();
+    let bot_name = "hold-my-beer-bot";
+
+    // start the bot
+    log::info!("Starting bot...");
+    teloxide::commands_repl(bot, bot_name, handler).await;
+
 }
